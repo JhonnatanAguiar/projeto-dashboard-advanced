a2ba1016732f1425852c44674f84057b13e2885d
Jhonnatan Aguiar <81327493+JhonnatanAguiar@users.noreply.github.com>
2026-03-11 17:42:38 +0000

    feat: criado componente Button

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,11 @@
+import Button from "../../components/ui/Button";
+
 export default function DashboardPage() {
   return (
     <div>
-      <h2 className="text-2x1 font-bold mb-4">Dashboard</h2>
+      <h2 className="text-2xl font-bold mb-4">Dashboard</h2>
       <p>Bem-vindo ao InsightFlow Analytics!</p>
+      <Button>Home</Button>
     </div>
   );
 }
